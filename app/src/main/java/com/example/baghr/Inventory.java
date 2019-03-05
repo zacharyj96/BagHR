508f6f615dd83a72825f55e3b1be3a8ba11d2137
Zachary Johnson <zacharyrjohnson96@gmail.com>
2019-03-05 14:42:12 -0800

    Added Create Account, Inventory, Main classes

---
+++ app/src/main/java/com/example/baghr/Inventory.java
@@ -0,0 +1,13 @@
+package com.example.baghr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Inventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
